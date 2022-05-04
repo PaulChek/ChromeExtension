18525afa67ac6455728a519dcc6c0a2eb1ac0d91
paul <paulchek777@gmail.com>
2022-05-05 05:43:47 +0700

    gdsfgsdgf

--- script_popup.js
+++ script_popup.js
@@ -1,11 +1,11 @@
-//// JavaScript source code
-document.addEventListener('DOMContentLoaded', function () {
-    let flag = true;
-    let btn = document.getElementById("btn1");
-    document.getElementById("btn1").addEventListener("click", _ => {
-        btn.innerText = flag ? "Cliked" : "Click";
-        btn.style.backgroundColor = !flag ? "white" : "orangered";
-        flag = !flag;
+((flag) => {
+    document.addEventListener("DOMContentLoaded", function () {
+        let btn = document.getElementById("btn1");
+        document.getElementById("btn1").addEventListener("click", _ => {
+            btn.innerText = flag ? "Cliked" : "Click";
+            btn.style.backgroundColor = !flag ? "white" : "orangered";
+            flag = !flag;
 
+        });
     });
-})
+})(true);
