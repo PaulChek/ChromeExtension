68747127f519ab1a68cf11cb1f5d23521bb9c362
paul <paulchek777@gmail.com>
2022-05-06 07:51:14 +0700

    fdgd

--- content_script.js
+++ content_script.js
@@ -1,7 +1,7 @@
 ﻿chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-
-        document.querySelector("body").style.backgroundColor = request.dark_flag ? "#333333" : "#3b3b3b";
-        console.log(request);
+        var my_color = request.dark_flag ? "#ff5722" : "#b6ff00";
+        document.querySelectorAll("span.DPvwYc,a.fIqyif.MdgzLb,path.ugRcF").forEach(v => (v.style.color = my_color, v.style.fill = my_color));
+        document.querySelectorAll("circle.zbdB4c,circle.iIDD2e").forEach(v => v.style.stroke = my_color);
     }
 );
